e192bce8581c92a85bb98b0e4c420b0681649bb0
jianhui xu <jianhui.xu@tendcloud.com>
2017-02-10 17:54:33 +0800

    Add Heros  List With Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ export class AppComponent {
     id:1,
     name:'windstorm'
   }
+  heros = HEROS
+
+
+
 }
 
 
@@ -18,3 +22,17 @@ export class Hero {
   id: number;
   name: string;
 }
+
+
+const HEROS: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+]
