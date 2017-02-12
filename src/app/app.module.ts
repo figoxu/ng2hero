08c2824c0a1be4f67d22b6055bb8a2937c8a41fd
figoxu <xujh945@gmail.com>
2017-02-12 17:02:43 +0800

    Add In Memory Rest Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HeroService} from "./hero.service";
+
 
 @NgModule({
   declarations: [
@@ -21,6 +25,8 @@ import {HeroService} from "./hero.service";
     BrowserModule,
     FormsModule,
     HttpModule,
+
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path:'heroes',
